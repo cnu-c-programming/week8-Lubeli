14b34c7189abc87a8953f59c464f2276b7e61b90
Lubeli <144096691+Lubeli@users.noreply.github.com>
2026-04-24 09:49:41 +0900

    Update 3-2.c

---
+++ code/3-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+
+    char *temp;
+    for(int i = argc-1; i>0; i--){
+        for(int j = 0; j<i; j++){
+            if(strcmp(argv[j], argv[j+1]) == 1){
+                temp = argv[j+1];
+                argv[j+1] = argv[j];
+                argv[j] = temp;
+            }
+        }
+    }
+
+    for(int i = 0; i < argc; i++){
+        printf("%s\n",argv[i]);
+    }
+    return 0;
+}
